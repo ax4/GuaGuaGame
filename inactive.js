d427390e9612b216ab912303b86c4e32e544ab10
ax4 <ax4@catcode.xyz>
2017-01-20 14:12:03 -0700

    Change Counter from Seconds into MilliSeconds

--- inactive.js
+++ inactive.js
@@ -1,28 +1,30 @@
 var IDLE_TIMEOUT = 10; //seconds
-var _idleSecondsCounter = 0;
+var _idleMilliSecondsCounter = 0; //in XXX ms
+
+
 document.onclick = function() {
-  _idleSecondsCounter = 0;
+  _idleMilliSecondsCounter = 0;
 };
 document.onmousemove = function() {
-  _idleSecondsCounter = 0;
+  _idleMilliSecondsCounter = 0;
 };
 document.onkeypress = function() {
-  _idleSecondsCounter = 0;
+  _idleMilliSecondsCounter = 0;
 };
 
 var myInterval = window.setInterval(CheckIdleTime, 1000);
 
 function CheckIdleTime() {
-  _idleSecondsCounter++;
+  _idleMilliSecondsCounter++;
   var oPanel = document.getElementById("SecondsUntilExpire");
   if (oPanel)
-    oPanel.innerHTML = (IDLE_TIMEOUT - _idleSecondsCounter) + "";
-  if (_idleSecondsCounter >= IDLE_TIMEOUT) {
+    oPanel.innerHTML = Math.round((IDLE_TIMEOUT*1000 - _idleMilliSecondsCounter)/1000) + "";
+  if (_idleMilliSecondsCounter >= IDLE_TIMEOUT*1000) {
     alert("Time expired!");
     window.clearInterval(myInterval);
     oPanel.innerHTML = ("Job Done");
     
-    _idleSecondsCounter = 0;
+    _idleMilliSecondsCounter = 0;
     myInterval = window.setInterval(CheckIdleTime, 1000);
   }
 }
