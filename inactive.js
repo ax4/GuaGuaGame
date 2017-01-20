b7220bcc7acd00cf4e28477ca62f4eaa5a960bbf
ax4 <ax4@catcode.xyz>
2017-01-20 14:46:10 -0700

    display fix

--- inactive.js
+++ inactive.js
@@ -22,7 +22,7 @@ function CheckIdleTime() {
   _idleMilliSecondsCounter+= _MilliSecondsInterval;
   var oPanel = document.getElementById("SecondsUntilExpire");
   if (oPanel)
-    oPanel.innerHTML = (IDLE_TIMEOUT*1000 - _idleMilliSecondsCounter)/1000 + " 秒，不动就死";
+    oPanel.innerHTML = (IDLE_TIMEOUT*1000 - _idleMilliSecondsCounter)/1000;
   if (_idleMilliSecondsCounter >= IDLE_TIMEOUT*1000) {
     alert("Time expired!");
     window.clearInterval(myInterval);
