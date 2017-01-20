d625c10099405821151005ed49f8f02598e6cc0f
ax4 <ax4@catcode.xyz>
2017-01-20 14:44:50 -0700

    chinese display

--- inactive.js
+++ inactive.js
@@ -22,7 +22,7 @@ function CheckIdleTime() {
   _idleMilliSecondsCounter+= _MilliSecondsInterval;
   var oPanel = document.getElementById("SecondsUntilExpire");
   if (oPanel)
-    oPanel.innerHTML = (IDLE_TIMEOUT*1000 - _idleMilliSecondsCounter)/1000 + "s";
+    oPanel.innerHTML = (IDLE_TIMEOUT*1000 - _idleMilliSecondsCounter)/1000 + " 秒，不动就死";
   if (_idleMilliSecondsCounter >= IDLE_TIMEOUT*1000) {
     alert("Time expired!");
     window.clearInterval(myInterval);
